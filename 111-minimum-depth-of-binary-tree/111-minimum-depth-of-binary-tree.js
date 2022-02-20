f5735d36414a24a0d19b183b89359f7fde6e4936
Yeongsan-Sonny <54147313+zeromountain@users.noreply.github.com>
2022-02-20 20:12:46 +0900

    Time: 318 ms (54.18%), Space: 98.3 MB (60.60%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -11,11 +11,24 @@
  * @return {number}
  */
 var minDepth = function(root) {
-    if(!root) return 0;
+    if (!root) return [];
     
-    if (!root.left) return minDepth(root.right) + 1;
-    if (!root.right) return minDepth(root.left) + 1;
+    let queue = [root];
+    let depth = 1;
     
-    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;   
+    while (queue[0]) {
+        let size = queue.length;
+        
+        while (size--) {
+            let curr = queue.shift(); // dequeue
+            if (!curr.left && !curr.right) return depth;
+            
+            if (curr.left) queue.push(curr.left); // enqueue
+            if (curr.right) queue.push(curr.right); // enqueue
+            
+        }
+        
+        depth++;
+    }
 };
 
