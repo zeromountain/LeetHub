ed4a683ae68dde7ed67b5512f719d9a617f0abf7
Yeongsan-Sonny <54147313+zeromountain@users.noreply.github.com>
2022-03-20 19:25:20 +0900

    Time: 103 ms (78.61%), Space: 46.2 MB (80.84%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+     let left = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            [nums[i], nums[left]] = [nums[left], nums[i]]; // swap
+            left++;
+        }
+    }
+	return nums;
+};
